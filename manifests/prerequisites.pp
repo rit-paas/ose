85736b805cc718d66b2cc304f7d4af3b6624998b
root <root@mgmxasnodet01.infra.rit-paas.com>
2015-09-01 14:13:05 +0200

    RHEL 7

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -1,5 +1,14 @@
 class ose::prerequisites {
 
+  exec { 'subscrption-manager':
+    command => 'subscription-manager repos --disable="*" && subscription-manager repos \
+      --enable="rhel-7-server-rpms" \
+      --enable="rhel-7-server-extras-rpms" \
+      --enable="rhel-7-server-optional-rpms" \
+      --enable="rhel-7-server-ose-3.0-rpms"',
+    path => '/usr/sbin/:/bin/',
+  }
+
   package { 'NetworkManager':
     ensure => purged,
     provider => 'yum',
