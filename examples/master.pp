6398d495983e0d7e2d54a39c326eb72996444b53
root <root@mgmxasws01.infra.rit-paas.com>
2015-11-04 15:38:09 +0100

    added dmz nodes

--- examples/master.pp
+++ examples/master.pp
@@ -13,7 +13,9 @@ class { 'ose':
             {'hostname' => 'mgmxasnoded01.infra.rit-paas.com', 'labels' => "{'region': 'primary', 'zone': 'east'}"},
             {'hostname' => 'mgmxasnoded02.infra.rit-paas.com', 'labels' => "{'region': 'primary', 'zone': 'east'}"},
             {'hostname' => 'mgmxasnoded03.infra.rit-paas.com', 'labels' => "{'region': 'primary', 'zone': 'west'}"},
+            {'hostname' => 'dmzxaslbd01.infra.rit-paas.com', 'labels' => "{'region': 'dmz', 'zone': 'west'}"},
+            {'hostname' => 'dmzxaslbd02.infra.rit-paas.com', 'labels' => "{'region': 'dmz', 'zone': 'east'}"},
            ],
-  ssh_key_to => [ 'mgmxasmasterd01.infra.rit-paas.com', 'mgmxasnoded01.infra.rit-paas.com', 'mgmxasnoded02.infra.rit-paas.com', 'mgmxasnoded03.infra.rit-paas.com', ],
+  ssh_key_to => [ 'mgmxasmasterd01.infra.rit-paas.com', 'mgmxasnoded01.infra.rit-paas.com', 'mgmxasnoded02.infra.rit-paas.com', 'mgmxasnoded03.infra.rit-paas.com', 'dmzxaslbd01.infra.rit-paas.com', 'dmzxaslbd02.infra.rit-paas.com', ],
   root_pw => 'SECRETPASSWORDFORROOT',
 }
